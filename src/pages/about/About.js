eaae6054b97d678ce0d6a1bff214c098339afd03
jensyfranco <jenmilafran@gmail.com>
2023-06-20 20:16:29 +0200

    feat: work on the sign up page layout and styling

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import Footer from "../../components/footer/Footer";
-import "./About.css";
+import styles from "./About.module.css";
 import portraitJensy from '../../assets/Self_Portrait_Jensy.jpg';
 
 
 function About() {
     return (
         <>
-            <section className="about-section">
-                <h1>About</h1>
-                <p className="quote">"Photography is a way of feeling, of touching, of loving. What you have caught on film is captured forever… It remembers little things, long after you have forgotten everything." - Aaron Siskind</p>
-                <div className="image-container">
-                <img src={portraitJensy} alt="Photographer" className="photographer-portrait" />
-                    <div className="sub-text-container">
-                        <p className="sub-text"><strong>send us an email:</strong> info@mmanalog.com</p>
-                        <p className="sub-text"><strong>instagram:</strong> @mmanalog</p>
+            <section className={styles['about-section']}>
+                <h1 className={styles['page-title']}>About</h1>
+                <p className={styles.quote}>"Photography is a way of feeling, of touching, of loving. What you have caught on film is captured forever… It remembers little things, long after you have forgotten everything." - Aaron Siskind</p>
+                <div className={styles['image-container']}>
+                <img src={portraitJensy} alt="Photographer Jensy" className={styles['photographer-portrait']} />
+                    <div className={styles['sub-text-container']}>
+                        <p className={styles['sub-text']}><strong>send us an email:</strong> photo@mmanalog.com</p>
+                        <p className={styles['sub-text']}><strong>instagram:</strong> @mmanalogphotography</p>
                     </div>
                 </div>
-                <div className="side-text-right">
+                <div className={styles['side-text-right']}>
                     <p>keep shooting film</p>
                 </div>
-                <div className="side-text-left">
+                <div className={styles['side-text-left']}>
                     <p>keep shooting film</p>
                 </div>
             </section>
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import coverImage from '../../assets/Florian_Weichert_1.jpg';
+import './Signup.css';
 
+const Signup = () => {
 
-function Signup() {
     return (
-        <section className="signup">
-            <h1>Create an account</h1>
-        </section>
+        <div className="signup-container">
+            <div className="left-section">
+                <img src={coverImage} alt="Clouds"/>
+            </div>
+            <div className="right-section">
+                <h1>Create an account</h1>
+                    <p className="sub-text">Welcome. Nice to see you!</p>
+            </div>
+        </div>
     )
 }
 

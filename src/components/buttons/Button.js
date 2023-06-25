d2d55446132d7fc8f21b6648b8be675271425038
jensyfranco <jenmilafran@gmail.com>
2023-06-25 21:24:30 +0200

    feat: work on sign up page, add form to page

--- src/components/buttons/Button.js
+++ src/components/buttons/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Button.module.css';
 
-function Button({children, color, clickHandler, disabled}) {
+const Button = ({children, color, clickHandler, disabled}) => {
     const buttonClassName = `${styles['main--button']} ${styles[color]}`;
 
     return (
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import coverImage from '../../assets/Florian_Weichert_1.jpg';
 import './Signup.css';
+import axios from 'axios';
+import Button from "../../components/buttons/Button";
 
-const Signup = () => {
+function Signup() {
+
+    const [name, setName] = useState('');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [error, toggleError] = useState(false);
+    const [loading, toggleLoading] = useState(false);
+
+    const source = axios.CancelToken.source();
+    const history = useNavigate;
+
+    useEffect(() => {
+        return function cleanup() {
+            source.cancel();
+        }
+    }, []);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        toggleError(false);
+        toggleLoading(true);
+
+        try {
+            // koppelen aan backend zodra backend gereed is
+            await axios.post('', {
+                name: name,
+                username: username,
+                email: email,
+                password: password,
+                }, {
+                cancelToken: source.token,
+                });
+
+            history.push('./login');
+        } catch(e) {
+            console.error(e);
+            toggleError(true);
+        }
+        toggleLoading(false);
+    }
+
 
     return (
         <div className="signup-container">
@@ -12,6 +57,57 @@ const Signup = () => {
             <div className="right-section">
                 <h1>Create an account</h1>
                     <p className="sub-text">Welcome. Nice to see you!</p>
+                <form className="signup-form" onSubmit={handleSubmit}>
+                    <label htmlFor="name-field">
+                        Name
+                        <input
+                            type="text"
+                            id="name-field"
+                            name="name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </label>
+                    <label htmlFor="username-field">
+                        Username
+                        <input
+                            type="text"
+                            id="username-field"
+                            name="username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </label>
+                    <label htmlFor="email-field">
+                        Email
+                        <input
+                            type="email"
+                            id="email-field"
+                            name="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </label>
+                    <label htmlFor="password-field">
+                        Password
+                        <input
+                            type="password"
+                            id="password-field"
+                            name="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </label>
+                    {error && <p className="error">Email already exist. Please try another email.</p>}
+                    <Button
+                        type="button"
+                        className="create-account"
+                        disabled={loading}
+                    >
+                        Create an account
+                    </Button>
+                </form>
+                <p>Already have an account? <Link to="/login">Log in</Link>!</p>
             </div>
         </div>
     )
